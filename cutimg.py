669067c15252ff85e763b8164d9541afa8aa2c75
dlfming <dlfming@gmail.com>
2021-03-01 14:33:35 +0800

    thrd commit

--- cutimg.py
+++ cutimg.py
@@ -1,6 +1,10 @@
 import cv2
 
-img = cv2.imread(r"img/a1.jpeg")
+img = cv2.imread(r"img/battery.jpg")
 
-cv2.imwrite(r"img/a01.jpeg", img[0:433, 0:316])
-cv2.imwrite(r"img/a02.jpeg", img[0:433, 324:640])
+img = img[255:365, 0:1055]
+
+img = cv2.copyMakeBorder(img, 300, 300, 300, 300, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+
+cv2.imwrite(r"img/battery_test.jpg", img)
+# cv2.imwrite(r"img/a02.jpeg", img[0:433, 324:640])
--- translateimg.py
+++ translateimg.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 
-img = cv.imread(r'img/battery.jpeg', 1)
+img = cv.imread(r'img/battery_test.jpg', 1)
 rows, cols, channels = img.shape
 p1 = np.float32([[0,0], [cols-1,0], [0,rows-1], [rows-1,cols-1]])
 p2 = np.float32([[0,rows*0.3], [cols*0.8,rows*0.2], [cols*0.15,rows*0.7], [cols*0.8,rows*0.8]])
